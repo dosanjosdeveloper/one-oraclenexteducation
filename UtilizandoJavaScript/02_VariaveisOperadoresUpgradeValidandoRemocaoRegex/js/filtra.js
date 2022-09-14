f724bc9403f9a162cc211ea96434eed153fee0cc
dosanjosdeveloper <rafael.dosanjos@outlook.com>
2022-09-14 01:18:29 -0300

    Integrando Via API

---
+++ UtilizandoJavaScript/02_VariaveisOperadoresUpgradeValidandoRemocaoRegex/js/filtra.js
@@ -0,0 +1,36 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function () {
+    //    console.log(campoFiltro.textContent);
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            //console.log(nome);
+
+            var expressao = new RegExp(this.value, "i");
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel")
+            }
+
+/*          Substituido por Regex
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel")
+            }
+            */
+        }
+    }else{
+        for(var i=0;i < pacientes.length ; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+})
